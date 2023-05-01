daa8a978fb53dffbb4e90ac3bbbde69cb35afd57
Mici7120 <christian.villanueva@correounivalle.edu.co>
2023-04-30 21:38:14 -0500

    add parallel and update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# mult_matrices
+# Multiplicacion de matrices
+
+<br>
+
+## **Seqential**
+
+`mult_matrices_sequential.c` does it in a sequential manner.
+```
+ gcc mult_matrices_sequential.c -o mult.out
+ ./mult.out
+```
+
+<br>
+
+## **Parallel**
+
+`mult_matrices_parallel.c` &ensp; divides generation/calculation of all the points use to make the pi stimation between all the threads
+```
+ gcc -fopenmp mult_matrices_parallel.c -o mult_parralel.out
+ ./mult_parralel.out
+```
+
+To specify the amount of threads you want to use, run:
+```
+ OMP_NUM_THREADS=12 ./mult_parralel.out
+```
---
+++ mult_matrices_sequential.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+#define DIM 1024
+
+void imprimir(double *m)
+{
+  for (int i = 0; i < DIM; i++)
+  {
+    for (int j = 0; j < DIM; j++)
+      printf("%8.2f ", *(m + i * DIM + j));
+    printf("\n");
+  }
+}
+void mult_mat(double *c, double *a, double *b)
+{
+  clock_t start_time, end_time;
+  double total_time;
+  start_time = clock();
+  int i, j, k;
+  for (i = 0; i < DIM; i++)
+  {
+    for (j = 0; j < DIM; j++)
+    {
+      *(c + i * DIM + j) = 0.0;
+      for (k = 0; k < DIM; k++)
+        // c[i][j] += a[i][k] * b[k][j];
+        *(c + i * DIM + j) += *(a + i * DIM + k) * *(b + k * DIM + j);
+    }
+  }
+  end_time = clock();
+  total_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+  printf("Total execution time: %lf seconds. \n", total_time);
+}
+
+int main(int argc, char *argv[])
+{
+  double *a, *b, *c;
+
+  a = (double *)malloc(sizeof(double) * DIM * DIM);
+  assert(a != NULL);
+  b = (double *)malloc(sizeof(double) * DIM * DIM);
+  assert(b != NULL);
+  c = (double *)malloc(sizeof(double) * DIM * DIM);
+  assert(c != NULL);
+  for (int i = 0; i < DIM; i++)
+  {
+    for (int j = 0; j < DIM; j++)
+    {
+      *(b + i * DIM + j) = i * DIM + j;
+      *(a + i * DIM + j) = 0.0;
+    }
+    *(a + i * DIM + i) = 1.0;
+  }
+  mult_mat(c, a, b);
+  // imprimir(c);
+  return 0;
+}
